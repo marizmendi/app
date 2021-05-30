0b7c8aa1447e3977de5dc67328f1ac9d32ee9237
Manuel M Arizmendi <23641126+marizmendi@users.noreply.github.com>
2021-05-30 11:20:55 -0500

    sum changes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var helmet = require('helmet')
+const express = require('express');
 
-var indexRouter = require('./routes/index');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const helmet = require('helmet')
+const logger = require('morgan');
+const path = require('path');
 
-var app = express();
+const indexRouter = require('./routes/index');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/ip', function(req, res, next) {
-  res.locals.ip = req.headers["cf-connecting-ip"]
+  res.locals.ip = req.ip
   res.render('ip');
 });
 
